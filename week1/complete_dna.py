b7dfe072a0695262eaa7b83c18d9e697b8556be4
uskovboris <uskov_b@mail.ru>
2019-03-04 23:43:13 +0300

    complement strand task, patterns positions find tesks

---
+++ week1/complete_dna.py
@@ -0,0 +1,33 @@
+import sys
+
+def main():
+
+	if len(sys.argv) !=2:
+		show_usage()
+
+	dataset_file = sys.argv[1]
+
+	with open(dataset_file) as f:
+		dataset = f.readlines()
+
+	if len(dataset) != 1:
+		print('Dataset should contains exactly 1 line with template strand')
+		sys.exit(1)
+
+	template_strand = dataset[0]
+	complement_strand = ""
+
+	for nucleotide in template_strand:
+		if nucleotide == 'A':
+			complement_strand = 'T' + complement_strand
+		elif nucleotide == 'T':
+			complement_strand = 'A' + complement_strand
+		elif nucleotide == 'G':
+			complement_strand = 'C' + complement_strand
+		elif nucleotide == 'C':
+			complement_strand = 'G' + complement_strand
+
+	print(complement_strand)
+
+if __name__ == '__main__':
+	main()
---
+++ week1/pattern_pos.py
@@ -0,0 +1,40 @@
+import sys
+
+def show_usage():
+	print("Usage:")
+	print("python pattern_count.py <dataset_file>")
+
+def pattern_pos(chromosome, pattern): 
+	result = ""
+	pattern_len = len(pattern)
+	for i in range(len(chromosome)-pattern_len):
+		if (chromosome[i:i+pattern_len] == pattern):
+			result += str(i) + ' '
+	return result
+
+def main():
+
+	if len(sys.argv) != 2:
+		show_usage()
+
+	dataset_file = sys.argv[1]
+
+	with open(dataset_file) as f:
+		dataset = f.readlines()
+
+	if len(dataset) != 2:
+		print('Dataset should contains 2 lines')
+		sys.exit(1)
+
+	chromosome = dataset[1].strip()
+	pattern = dataset[0].strip()
+
+	print("chromosome: %s" % chromosome)
+	print("pattern: %s" % pattern)
+
+	output = pattern_pos(chromosome, pattern)
+	with open('./output.txt', "w") as f:
+		f.write(output)
+
+if __name__ == '__main__':
+	main()
